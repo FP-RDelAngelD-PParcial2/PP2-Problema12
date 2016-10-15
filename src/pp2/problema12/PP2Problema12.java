6453c589e3566c0a268bd2151d189cc91396ef7e
PC <PC@192.168.1.70>
2016-10-15 15:16:08 -0500

    12.	Realiza un programa que reciba 10 números enteros por teclado y guardarlos en un arreglo, después calcula y muestra la media de los números que estén en las posiciones pares del arreglo. La división de la posición entre 2 debe dar residuo 0.

--- src/pp2/problema12/PP2Problema12.java
+++ src/pp2/problema12/PP2Problema12.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema12;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,25 @@ public class PP2Problema12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[]numeros = new int[9];
+        mostrarVector(llenarVector(numeros));
     }
-    
+        public static int [] llenarVector(int []n){//Metodo de llenar el vector, recibe un arreglo de enteros, regresa un arreglo de enteros
+        Scanner entradaEscaner = new Scanner(System.in);
+        
+        for (int i = 0; i<n.length; i++){
+            System.out.print("Introduce un número para la posición [" + i + "]"); //i inidica la posición
+            n[i]=entradaEscaner.nextInt();
+        }
+        return n;
+       }
+        public static void mostrarVector(int []n){
+        double promedio;
+        promedio=0;
+            for(int i=0; i<n.length; i++){ //aumenta 1 cada vez
+            if(i%2==0){
+                promedio=promedio+n[i];
+            }
+        }
+        }
 }
